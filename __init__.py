5fb297ee11415aede8c1764c2cb9e9e969b7f511
Gabriel Fonseca <gabrielfonseca91@hotmail.com>
2018-05-20 20:47:06 -0300

    Adding subplot function

--- __init__.py
+++ __init__.py
@@ -1,12 +1,19 @@
-import cvs_temperature
+import csv_temperature
 import temperature_plot
+from matplotlib import pyplot as plt
 
-filename = 'sitka_weather_2014.csv'
+filename1 = 'sitka_weather_2014.csv'
+filename2 = 'death_valley_2014.csv'
 print('----------------------------------------------------------------------')
 option = input('Do you want to see the chart in Celcius (c) ot Farenheit (f): ')
 if option.title() == 'C':
-    dates, highs_temp, lows_temp = cvs_temperature.convert_temperature(filename)
-    temperature_plot.plot_temperature(highs_temp, lows_temp, dates, option)
+    dates1, highs_temp1, lows_temp1 = csv_temperature.convert_temperature(filename1)
+    dates2, highs_temp2, lows_temp2 = csv_temperature.convert_temperature(filename2)
+    temperature_plot.subplot_temp(filename1, filename2, option)
 if option.title() == 'F':
-    dates, highs_temp, lows_temp = cvs_temperature.get_highs_lows(filename)
-    temperature_plot.plot_temperature(highs_temp, lows_temp, dates, option)
+    dates1, highs_temp1, lows_temp1 = csv_temperature.get_highs_lows(filename1)
+    dates2, highs_temp2, lows_temp2 = csv_temperature.get_highs_lows(filename2)
+    temperature_plot.subplot_temp(filename1, filename2, option)
+else:
+    print('Comando inválido o programa será encerrado!')
+    print('----------------------------------------------------------------------')
--- temperature_plot.py
+++ temperature_plot.py
@@ -1,4 +1,4 @@
-import cvs_temperature
+import csv_temperature
 from matplotlib import pyplot as plt
 
 
@@ -15,5 +15,40 @@ def plot_temperature(highs_temp, lows_temp, dates, unit):
     plt.ylabel('Temperature (°{})'.format(unit.title()), fontsize=12)
     plt.tick_params(axis='both', labelsize=10)
     plt.xlim(dates[0], dates[-1])
+    plt.grid()
+    plt.show()
+
+
+def subplot_temp(filename1, filename2, unit):
+    if unit.title() == 'C':
+        dates1, highs1, lows1 = csv_temperature.convert_temperature(filename1)
+        dates2, highs2, lows2 = csv_temperature.convert_temperature(filename2)
+    if unit.title() == 'F':
+        dates1, highs1, lows1 = csv_temperature.get_highs_lows(filename1)
+        dates2, highs2, lows2 = csv_temperature.get_highs_lows(filename2)
+    fig = plt.figure(dpi=128, figsize=(10, 6))
+
+    ax1 = plt.subplot(211)
+    plt.plot(dates1, highs1, c='red', alpha=0.5)
+    plt.plot(dates1, lows1, c='blue', alpha=0.5)
+    plt.fill_between(dates1, highs1, lows1, facecolor='green', alpha=0.1)
+    plt.setp(ax1.get_xticklabels(), visible=False)
+    plt.title('Daily high and low temperatures Sitka - 2014', fontsize=16)
+    fig.autofmt_xdate()
+    plt.ylabel('Temperature (°{})'.format(unit.title()), fontsize=12)
+    plt.tick_params(axis='both', labelsize=10)
+    plt.xlim(dates1[0], dates1[-1])
+    plt.grid()
+
+    ax2 = plt.subplot(212, sharex=ax1)
+    plt.plot(dates2, highs2, c='red', alpha=0.5)
+    plt.plot(dates2, lows2, c='blue', alpha=0.5)
+    plt.fill_between(dates2, highs2, lows2, facecolor='green', alpha=0.1)
+    plt.setp(ax1.get_xticklabels(), fontsize=14)
+    plt.title('Daily high and low temperatures Death Valley - 2014', fontsize=16)
+    fig.autofmt_xdate()
+    plt.ylabel('Temperature (°{})'.format(unit.title()), fontsize=12)
+    plt.tick_params(axis='both', labelsize=10)
+    plt.xlim(dates2[0], dates2[-1])
     plt.grid()
     plt.show()
